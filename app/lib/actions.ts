5cd5bc24ac141f14ef054b5c5340a050650c25cf
siva <siva636@gmail.com>
2025-05-31 06:19:51 +0530

    Form validation and accessibility

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -8,21 +8,44 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string( {
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce.number().gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData:FormData) {
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 
-   const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(prevState: State, formData:FormData) {
+
+   // Validate form fields using Zod
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
    const amountInCents = amount * 100;
    const date = new Date().toISOString().split('T')[0]
 
@@ -32,8 +55,10 @@ export async function createInvoice(formData:FormData) {
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
 } catch(e){
-  // handle error
-  return;
+  // If a database error occurs, return a more specific error.
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    };
 }
 
   revalidatePath('/dashboard/invoices');
@@ -60,8 +85,11 @@ export async function updateInvoice(id: string, formData: FormData) {
     WHERE id = ${id}
   `;
 }catch(e){
-  // handle error
-  return;}
+ // If a database error occurs, return a more specific error.
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    };
+}
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
@@ -72,8 +100,10 @@ export async function deleteInvoice(id: string) {
   try{
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   }catch(e){
-    // handle error
-    return;
+  // If a database error occurs, return a more specific error.
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    };
   }
   revalidatePath('/dashboard/invoices');
 }
